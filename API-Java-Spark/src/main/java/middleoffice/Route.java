562885b03d58ef0c69916560fc0f9fd39a21491f
kui26 <kui.stephane@gmail.com>
2018-01-19 17:00:44 +0100

    formulaire de création de la demande

--- API-Java-Spark/src/main/java/middleoffice/Route.java
+++ API-Java-Spark/src/main/java/middleoffice/Route.java
@@ -1,47 +1,74 @@
 package middleoffice;
 
 import static spark.Spark.*;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.json.JSONObject;
 
 public class Route {
+	public String demandesCreation() {
+		String content = "";
+		try {
+			BufferedReader in = new BufferedReader(new FileReader("Web/demandesCreation.html"));
+			String str;
+			while ((str = in.readLine()) != null) {
+				content += str;
+			}
+			in.close();
+		} catch (IOException e) {
+			System.err.println("Route::demandesCreation Exception : ");
+			System.err.println(e);
+		}
 
-    public static void main(String[] args) {
-        setPort(80);
+		return content;
+	}
 
-        get("/", (request, response) -> {
-            return "GET /";
-        });
+	public static void main(String[] args) {
+		Route routing = new Route();
+		setPort(80);
 
-        post("/demandes", (request, response) -> {
-            return "Test OK";
-        });
+		get("/", (request, response) -> {
+			return "GET /";
+		});
 
-        put("/demandes/:id", (request, response) -> {
-            return "Test OK " + request.params(":id");
-        });
+		get("/demandes", (request, response) -> {
+			JSONObject obj = new JSONObject();
 
-        get("/demandes", (request, response) -> {
-          JSONObject obj = new JSONObject();
+			obj.put("name", "foo");
+			// obj.put("num", new Integer(100));
+			// obj.put("balance", new Double(1000.21));
+			// obj.put("is_vip", new Boolean(true));
 
-          obj.put("name", "foo");
-          // obj.put("num", new Integer(100));
-          // obj.put("balance", new Double(1000.21));
-          // obj.put("is_vip", new Boolean(true));
+			return obj;
+		});
 
-          return obj;
-        });
+		// Creation formulaire
+		get("/demandes/creation", (request, response) -> {
+			return routing.demandesCreation();
+		});
+		
+		post("/demandes", (request, response) -> {
+			return "Test OK";
+		});
 
-        get("/demandes/:id", (request, response) -> {
+		put("/demandes/:id", (request, response) -> {
+			return "Test OK " + request.params(":id");
+		});
 
-          return "Test OK" + request.params(":id");
-        });
+		post("/demandes/:id", (request, response) -> {
+			String vote = request.queryParams("vote");
 
-        post("/demandes/:id", (request, response) -> {
-          String vote = request.queryParams("vote");
+			String json = "{'demandes':{'id':3}}";
 
-          String json = "{'demandes':{'id':3}}";
+			return "paramId=" + request.params(":id") + "; vote=" + vote + " ; json=" + json;
+		});
 
-          return "paramId=" + request.params(":id") + "; vote=" + vote +" ; json="+json;
-        });
-    }
+		get("/demandes/:id", (request, response) -> {
+
+			return "Test OK" + request.params(":id");
+		});
+	}
 }
